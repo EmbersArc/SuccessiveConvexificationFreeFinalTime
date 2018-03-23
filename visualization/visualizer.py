b4ddaa049d4753d92e125a2743218e8e3a5656cf
EmbersArc <s-niederberger@hotmail.com>
2018-03-23 16:59:25 +0100

    optimizations

--- visualization/visualizer.py
+++ visualization/visualizer.py
@@ -3,15 +3,6 @@ import pickle
 import os
 import bpy
 
-
-def create_ranges_nd(start, stop, N, endpoint=True):
-    if endpoint == 1:
-        divisor = N - 1
-    else:
-        divisor = N
-    steps = (1.0 / divisor) * (stop - start)
-    return start[..., None] + steps[..., None] * np.arange(N)
-
 abspath = os.path.abspath(os.path.dirname(__file__))
 dname = os.path.dirname(abspath)
 os.chdir(dname)
@@ -20,17 +11,11 @@ rck = bpy.data.objects["rck"]
 eng = bpy.data.objects["eng"]
 fir = bpy.data.objects["fir"]
 
-X_in = pickle.load(open("trajectory/X.p", "rb"))
-U_in = pickle.load(open("trajectory/U.p", "rb"))
+X = pickle.load(open("trajectory/X.p", "rb"))
+U = pickle.load(open("trajectory/U.p", "rb"))
+sigma = pickle.load(open("trajectory/sigma.p", "rb"))
 
-FPS = 10  # not really FPS, but frames per discretization step
-
-X = np.empty((len(X_in) * FPS, 14))
-U = np.empty((len(U_in) * FPS, 3))
-
-for i in range(len(X_in) - 1):
-    X[i * FPS:(i + 1) * FPS, :] = create_ranges_nd(X_in[i, :], X_in[i + 1, :], FPS).T
-    U[i * FPS:(i + 1) * FPS, :] = create_ranges_nd(U_in[i, :], U_in[i + 1, :], FPS).T
+FPS = int(sigma * 1.5) # frames per discretization step
 
 bpy.data.scenes["Scene"].frame_current = 1
 
@@ -38,7 +23,12 @@ rck.animation_data_clear()
 eng.animation_data_clear()
 fir.animation_data_clear()
 
-for i in range(len(U) - FPS):
+fir.hide_render = False
+fir.keyframe_insert(data_path='hide_render')
+fir.hide = False
+fir.keyframe_insert(data_path='hide')
+
+for i in range(len(X)):
     x = X[i, :]
     u = U[i, :]
     rck.location = np.array((x[2], x[3], x[1]))
@@ -48,15 +38,21 @@ for i in range(len(U) - FPS):
     ry = -np.arctan(u[2] / u[0])
     rx = np.arctan(u[1] / u[0])
     eng.rotation_euler = (rx, ry, 0)
-    fir.scale[2] = throttle / 4.
+    fir.scale[2] = throttle
 
     rck.keyframe_insert(data_path='location')
     rck.keyframe_insert(data_path='rotation_quaternion')
     eng.keyframe_insert(data_path='rotation_euler')
     fir.keyframe_insert(data_path='scale')
-    bpy.data.scenes["Scene"].frame_current += 1
+
+    bpy.data.scenes["Scene"].frame_current += FPS
+    
+fir.hide_render = True
+fir.keyframe_insert(data_path='hide_render')
+fir.hide = True
+fir.keyframe_insert(data_path='hide')
 
 bpy.data.scenes["Scene"].frame_current = 1
-bpy.data.scenes["Scene"].frame_end = len(U) + 2 * FPS
+bpy.data.scenes["Scene"].frame_end = len(U) * FPS + 60
 
-print("Final mass:", X_in[-1, 0] / 10)
+print("Final mass:", X[-1, 0])
